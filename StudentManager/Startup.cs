7ddbdce7ca7780b172b160c16d0fdc5570b0de62
phuonglvd00631 <phuonglvd00631@fpt.edu.vn>
2021-03-01 20:00:07 +0700

    initProject

---
+++ StudentManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentManager.Startup))]
+namespace StudentManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
